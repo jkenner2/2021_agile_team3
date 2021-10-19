de6dabdd0cc0b1c789d2109977e0fce5395b06dd
jkenner2 <jkenner2@student.cvtc.edu>
2021-10-19 10:44:02 -0500

    [finishes #180000152] Added var to input variable to make it a local variable

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,5 @@
 // Convert the calculator face to use stand units of mesurement
-function standard () {
+function onStandardClick () {
   // Store labels
   var lblWeight = document.getElementById('lblWeight');
   var lblLargerHeight = document.getElementById('lblLargerHeight');
@@ -36,7 +36,7 @@ function standard () {
 }
 
 // Convert the calculator face to use metric units of mesurement
-function metric () {
+function onMetricClick () {
   // Store element labels
   var lblWeight = document.getElementById('lblWeight');
   var lblLargerHeight = document.getElementById('lblLargerHeight');
@@ -53,7 +53,7 @@ function metric () {
   lblSmallerHeight.innerHTML = "Centimeters:";
 
   // Get values
-  input = getRefenceToTextFields();
+  var input = getRefenceToTextFields();
 
   // Format
   var weight = parseFloat(input[0].value);
@@ -153,7 +153,7 @@ function calculateBMR() {
   var standard = document.getElementById('standard');
 
   // Get reference to male radio button
-  var male = document.getElementById('male');
+  var male_gender = document.getElementById('male');
 
   // Member variable to hold final calculation
   var bmr = null;
@@ -162,7 +162,7 @@ function calculateBMR() {
   if (standard.checked) {
     // Its checked, use standard units
     // Determine which equation to use by seeing if the male radio button is checked
-    if (male.checked) {
+    if (male_gender.checked) {
       // Its checked, use male calculation
       bmr = 66 + (6.23 * weight) + (12.7 * (convertFeetToInches(largerHeight) + smallerHeight)) - (6.8 * age);
 
@@ -173,7 +173,7 @@ function calculateBMR() {
   } else {
     // Its not checked, use metric units
     // Determine which equation to use by seeing if the male radio button is checked
-    if (male.checked) {
+    if (male_gender.checked) {
       // Its checked, use male calculation
       bmr = 66 + (13.7 * weight) + (5 * (convertMetersToCentimeters(largerHeight) + smallerHeight)) - (6.8 * age);
     } else {
@@ -367,93 +367,5 @@ function onSubmitClick() {
   var targetHR = targetHeartRate(maxHR);
 
   // Display the calculated information back to the user
-  displayAnswer ("Your BMI is " + bmi + ", and this shows that you are " + healthy + ". Your BMR is " + bmr + ". Your daily calorie burn is aproximatley " + active + ". Your max heart rate is " + maxHR + ". While working out, try to keep your heart reate between " + targetHR[0] + " and " + targetHR[1] + ".");
-}
-
-// Only called once submit button is clicked
-function calculateBMI() {
-  // Get reference weight and height inputs
-  const weight = parseFloat($("#weight")).value);
-  const heightFT = parseFloat(document.getElementById('heightFT').value);
-  const heightIN = parseFloat(document.getElementById('heightIN').value);
-
-  // Get reference to units
-  const standard = document.getElementById('standard');
-
-  var bmi;
-
-  // Determine units to use
-  if (standard.checked) {
-    // Use standard units
-    bmi = (weight / (Math.pow((convertFeetToInches(heightFT) + heightIN), 2))) * 703;
-  } else {
-    // Use metric units
-    bmi = weight / Math.pow((heightIN / 100) + heightFT, 2);
-  }
-
-  // Call function to answer on page and check for healthy bmi
-  if (bmi < 18.5) {
-  // Display if underweight
-     displayAnswer (bmi + " Underweight");
-  } else if (18.5 < bmi && bmi < 24.9) {
-  // Display if healthy
-     displayAnswer (bmi + " Healthy");
-  } else if (25.0 < bmi && bmi < 29.9) {
-  // Display if overweight
-     displayAnswer (bmi + " Overweight");
-  } else {
-  // Default display obese
-     displayAnswer (bmi + " Obese");
-  }
-}
-
-// Convert feet to inches
-function convertFeetToInches (mesurementInFeet) {
-  return mesurementInFeet * 12;
-}
-
-// Ensure that answer is displayed only once
-function displayAnswer (textToDisplay) {
-  const answerDiv = document.getElementById("answer");
-
-  // Delete old answer if exits
-  if (answerDiv.hasChildNodes()){
-    answerDiv.removeChild(answerDiv.firstChild);
-  }
-
-  // Create new h3 to show pybl
-  const newH3 = document.createElement('h3');
-
-  // form string to show pybl
-  var answer = document.createTextNode("BMI: " + String(textToDisplay));
-
-  // attach to h3
-  newH3.appendChild(answer);
-
-  // attach h3 with pybl to div
-  answerDiv.appendChild(newH3);
-}
-
-function standard () {
-  // Store labels
-  const lblWeight = document.getElementById('lblWeight');
-  const lblHeight = document.getElementById('lblHeight');
-  const lblHeight2 = document.getElementById('lblHeight2');
-
-  // Convert units to metric
-  lblWeight.innerHTML = "Weight(lbs):";
-  lblHeight.innerHTML = "Feet:";
-  lblHeight2.innerHTML = "Inches:";
-}
-
-function metric () {
-  // Store labels
-  const lblWeight = document.getElementById('lblWeight');
-  const lblHeight = document.getElementById('lblHeight');
-  const lblHeight2 = document.getElementById('lblHeight2');
-
-  // Convert units to metric
-  lblWeight.innerHTML = "Weight(kg):";
-  lblHeight.innerHTML = "Meters:";
-  lblHeight2.innerHTML = "Centimeters:";
+  displayAnswer ("Your BMI is " + bmi + ", and this shows that you are " + healthy + ". Your BMR is " + bmr + ". Your daily calorie burn is aproximatley " + active + ". Your max heart rate is " + maxHR + ". While working out, try to keep your heart rate between " + targetHR[0] + " and " + targetHR[1] + ".");
 }
